bdb033300f72c7357d31ebd6f64b206c8809f51f
AR-Shahin <mdshahinmije96@gmail.com>
2021-11-20 18:46:56 +0600

    RB algo solve seperate function

--- Algorithm/RB/1st/Assignment/ass.cpp
+++ Algorithm/RB/1st/Assignment/ass.cpp
@@ -1,17 +1,42 @@
 
 #include <bits/stdc++.h>
+#define WIDTH 2
 using namespace std;
 
+void bubble_sort(int arr[][WIDTH], int HEIGHT)
+{
+    for (int i = 0; i < HEIGHT - 1; i++)
+    {
+        for (int j = 0; j < HEIGHT - i - 1; j++)
+        {
+
+            /* cout<<arr[j][0]<<" " <<arr[j+1][0]<<endl;*/
+            if (arr[j][0] < arr[j + 1][0])
+            {
+                swap(arr[j][0], arr[j + 1][0]);
+                swap(arr[j][1], arr[j + 1][1]);
+            }
+            else if (arr[j][0] == arr[j + 1][0])
+            {
+                if (arr[j][1] < arr[j + 1][1])
+                {
+                    swap(arr[j][0], arr[j + 1][0]);
+                    swap(arr[j][1], arr[j + 1][1]);
+                }
+            }
+        }
+    }
+}
 int main()
 {
 
     clock_t t1 = clock();
 
-    int HEIGHT, WIDTH = 2; //declaring
+    int HEIGHT = 2; //declaring
 
     /* Code to read in txt file */
     ifstream myFile;
-    myFile.open("test.txt");
+    myFile.open("input.txt");
 
     if (!myFile)
     {
@@ -39,28 +64,8 @@ int main()
     }
 */
 
-    for (int i = 0; i < HEIGHT - 1; i++)
-    {
-        for (int j = 0; j < HEIGHT - i - 1; j++)
-        {
+    bubble_sort(arr, HEIGHT);
 
-            cout << arr[j][0] << " " << arr[j + 1][0] << endl;
-            if (arr[j][0] < arr[j + 1][0])
-            {
-                swap(arr[j][0], arr[j + 1][0]);
-                swap(arr[j][1], arr[j + 1][1]);
-            }
-            else if (arr[j][0] == arr[j + 1][0])
-            {
-                if (arr[j][1] < arr[j + 1][1])
-                {
-                    swap(arr[j][0], arr[j + 1][0]);
-                    swap(arr[j][1], arr[j + 1][1]);
-                }
-            }
-        }
-    }
-    /*
     freopen("output.txt", "w", stdout);
     for (int j = 0; j < HEIGHT; j++)
     {
@@ -69,6 +74,6 @@ int main()
     clock_t t2 = clock();
     clock_t t = (t2 - t1) / CLOCKS_PER_SEC;
     cout << "total time =" << t << endl;
-*/
+
     return 0;
 }
