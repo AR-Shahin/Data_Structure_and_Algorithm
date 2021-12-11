6d3ec796c6a8cf56fcbafdb68c34e5c81c6d2200
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-11 10:36:15 +0600

    implement interpolation serach

---
+++ Algorithm/RB/searching/interpolation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int interpolation_search(int A[], int low, int high, int val)
+{
+    if (high >= low)
+    {
+        int mid = (low + ((val - A[low]) * (high - low)) / (A[high] - A[low]));
+        if (A[mid] == val)
+        {
+            return mid;
+        }
+        else if (A[mid] > val)
+        {
+            return interpolation_search(A, low, mid, val);
+        }
+        else if (A[mid] < val)
+        {
+            return interpolation_search(A, mid + 1, high, val);
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int A[] = {-8, -1, 5, 9, 10, 17, 18, 25, 36};
+    int n = 9;
+    int val = -8;
+    int pos = interpolation_search(A, 0, n - 1, val);
+    cout << pos;
+    return 0;
+}
