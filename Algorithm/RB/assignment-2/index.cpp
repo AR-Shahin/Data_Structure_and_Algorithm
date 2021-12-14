acb1f13d7d3cabee3e8e1d4ef48eff6c91b17288
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-14 18:36:02 +0600

    assignmenr tried

--- Algorithm/RB/assignment-2/index.cpp
+++ Algorithm/RB/assignment-2/index.cpp
@@ -368,6 +368,207 @@ int main()
     }
 
     // ------------- Quick Sort Dsc End : -----------
+    /*
+        // File : 2
+        double F2Asc[10];
+        double F2Dsc[10];
+        ifstream myFile2;
+        // myFile.open("input1.txt");
+        myFile2.open("i2.txt");
+
+        int n2;
+        myFile2 >> n2;
+        int C[n2];
+        // Read File Two
+        for (int i = 0; i < n2; i++)
+        {
+            myFile >> C[i];
+        }
+        myFile2.close();
+
+        // Bubble Sort Asc Start
+        clock_t F2_bbAscClockStart = clock();
+        bubble_sort_asc(C, n1);
+        clock_t F2_bbAscClockEnd = clock();
+        double F2_bbAscTime = (F2_bbAscClockEnd - F2_bbAscClockStart) / CLOCKS_PER_SEC;
+        F2Asc[0] = bbAscTime;
+        freopen("i2_bubble_asc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+
+        // ---- Bubble Sort Asc End : ---
+
+        // Bubble Sort Dsc Start
+        clock_t F2_bbDscClockStart = clock();
+        bubble_sort_dsc(C, n2);
+        clock_t F2_bbDscClockEnd = clock();
+        double F2_bbDscTime = (F2_bbDscClockEnd - F2_bbDscClockStart) / CLOCKS_PER_SEC;
+        F2Dsc[0] = F2_bbDscTime;
+        freopen("i2_bubble_dsc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+
+        // ----- Bubble Sort Dsc End : -----
+
+        // Merge Sort ASC Start
+        clock_t F2_mgAscClockStart = clock();
+        merge_sort_asc(C, 0, n2 - 1);
+        clock_t F2_mgAscClockEnd = clock();
+        double F2_mgAscTime = (F2_mgAscClockEnd - F2_mgAscClockStart) / CLOCKS_PER_SEC;
+        F2Asc[1] = F2_mgAscTime;
+        freopen("i2_merge_asc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+
+        // ------- Merge Sort Asc End : --------------
+
+        // Merge Sort DSC Start
+        clock_t F2_mgDscClockStart = clock();
+        merge_sort_dsc(C, 0, n2 - 1);
+        clock_t F2_mgDscClockEnd = clock();
+        double F2_mgDscTime = (F2_mgDscClockEnd - F2_mgDscClockStart) / CLOCKS_PER_SEC;
+        F2Dsc[1] = F2_mgDscTime;
+        freopen("i2_merge_dsc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+
+        // ------- Quick Sort Dsc End : -----------------
+
+        // Quick Sort ASC Start
+        clock_t F2_qsAscClockStart = clock();
+        quick_sort_asc(C, 0, n2 - 1);
+        clock_t F2_qsAscClockEnd = clock();
+        double F2_qsAscTime = (F2_qsAscClockEnd - F2_qsAscClockStart) / CLOCKS_PER_SEC;
+        F2Asc[2] = F2_qsAscTime;
+        freopen("i2_quick_asc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+
+        // ------------- Quick Sort Asc End : -----
+
+        // Quick Sort DSC Start
+        clock_t F2_qsDscClockStart = clock();
+        quick_sort_dsc(C, 0, n2 - 1);
+        clock_t F2_qsDscClockEnd = clock();
+        double F2_qsDscTime = (F2_qsDscClockEnd - F2_qsDscClockStart) / CLOCKS_PER_SEC;
+        F2Dsc[2] = F2_qsDscTime;
+        freopen("i2_quick_dsc.txt", "w", stdout);
+        for (int i = 0; i < n2; i++)
+        {
+            cout << C[i] << "\t";
+        }
+        */
+    /*
+        // File : 3
+        double F3Asc[10];
+        double F3Dsc[10];
+        ifstream myFile3;
+        // myFile.open("input1.txt");
+        myFile3.open("i3.txt");
+
+        int n3;
+        myFile3 >> n3;
+        int D[n3];
+        // Read File Two
+        for (int i = 0; i < n3; i++)
+        {
+            myFile >> D[i];
+        }
+        myFile3.close();
+
+        // Bubble Sort Asc Start
+        clock_t F3_bbAscClockStart = clock();
+        bubble_sort_asc(D, n3);
+        clock_t F3_bbAscClockEnd = clock();
+        double F3_bbAscTime = (F3_bbAscClockEnd - F3_bbAscClockStart) / CLOCKS_PER_SEC;
+        F3Asc[0] = bbAscTime;
+        freopen("i3_bubble_asc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+
+        // ---- Bubble Sort Asc End : ---
+
+        // Bubble Sort Dsc Start
+        clock_t F3_bbDscClockStart = clock();
+        bubble_sort_dsc(D, n3);
+        clock_t F3_bbDscClockEnd = clock();
+        double F3_bbDscTime = (F3_bbDscClockEnd - F3_bbDscClockStart) / CLOCKS_PER_SEC;
+        F3Dsc[0] = F3_bbDscTime;
+        freopen("i3_bubble_dsc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+
+        // ----- Bubble Sort Dsc End : -----
+
+        // Merge Sort ASC Start
+        clock_t F3_mgAscClockStart = clock();
+        merge_sort_asc(D, 0, n3 - 1);
+        clock_t F3_mgAscClockEnd = clock();
+        double F3_mgAscTime = (F3_mgAscClockEnd - F3_mgAscClockStart) / CLOCKS_PER_SEC;
+        F3Asc[1] = F3_mgAscTime;
+        freopen("i3_merge_asc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+
+        // ------- Merge Sort Asc End : --------------
+
+        // Merge Sort DSC Start
+        clock_t F3_mgDscClockStart = clock();
+        merge_sort_dsc(D, 0, n3 - 1);
+        clock_t F3_mgDscClockEnd = clock();
+        double F3_mgDscTime = (F3_mgDscClockEnd - F3_mgDscClockStart) / CLOCKS_PER_SEC;
+        F3Dsc[1] = F3_mgDscTime;
+        freopen("i3_merge_dsc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+
+        // ------- Quick Sort Dsc End : -----------------
+
+        // Quick Sort ASC Start
+        clock_t F3_qsAscClockStart = clock();
+        quick_sort_asc(D, 0, n3 - 1);
+        clock_t F3_qsAscClockEnd = clock();
+        double F3_qsAscTime = (F3_qsAscClockEnd - F3_qsAscClockStart) / CLOCKS_PER_SEC;
+        F3Asc[2] = F3_qsAscTime;
+        freopen("i3_quick_asc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+
+        // ------------- Quick Sort Asc End : -----
+
+        // Quick Sort DSC Start
+        clock_t F3_qsDscClockStart = clock();
+        quick_sort_dsc(D, 0, n3 - 1);
+        clock_t F3_qsDscClockEnd = clock();
+        double F3_qsDscTime = (F3_qsDscClockEnd - F3_qsDscClockStart) / CLOCKS_PER_SEC;
+        F3Dsc[2] = F3_qsDscTime;
+        freopen("i3_quick_dsc.txt", "w", stdout);
+        for (int i = 0; i < n3; i++)
+        {
+            cout << D[i] << "\t";
+        }
+    */
+    // ------------- Quick Sort Dsc End : -----------
 
     // int x[] = {-7, 8, 10};
     //  Final Result :
@@ -378,7 +579,22 @@ int main()
     cout << "Merge sort : ASC = " << mgAscTime << "S DSC = " << mgDscTime << "S" << endl;
     cout << "Quick sort : ASC = " << qsAscTime << "S DSC = " << qsDscTime << "S" << endl;
     cout << "Best Sorting : ASC = " << get_best_result(F1Asc, 3) << " Sort DSC = " << get_best_result(F1Dsc, 3) << " Sort";
-    // cout << get_best_result(x, 4);
-
+    cout << endl; /*
+     cout << "\n---------------------" << endl;
+     cout << "Input 2 :" << endl;
+     cout << "Bubble sort : ASC = " << F2_bbAscTime << "S DSC = " << F2_bbDscTime << "S" << endl;
+     cout << "Merge sort : ASC = " << F2_mgAscTime << "S DSC = " << F2_mgDscTime << "S" << endl;
+     cout << "Quick sort : ASC = " << F2_qsAscTime << "S DSC = " << F2_qsDscTime << "S" << endl;
+     cout << "Best Sorting : ASC = " << get_best_result(F2Asc, 3) << " Sort DSC = " << get_best_result(F2Dsc, 3) << " Sort";
+     cout << endl;
+     cout << "\n---------------------" << endl;
+     */           /*
+               cout << "Input 3 :" << endl;
+               cout << "Bubble sort : ASC = " << F3_bbAscTime << "S DSC = " << F3_bbDscTime << "S" << endl;
+               cout << "Merge sort : ASC = " << F3_mgAscTime << "S DSC = " << F3_mgDscTime << "S" << endl;
+               cout << "Quick sort : ASC = " << F3_qsAscTime << "S DSC = " << F3_qsDscTime << "S" << endl;
+               cout << "Best Sorting : ASC = " << get_best_result(F3Asc, 3) << " Sort DSC = " << get_best_result(F3Dsc, 3) << " Sort";
+               // cout << get_best_result(x, 4);
+           */
     return 0;
 }
